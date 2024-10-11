6811cb7f7ddf96f898ed3969da49ec792eb14f89
williamvillacrez <william.villacrez89@gmail.com>
2024-10-11 10:24:08 -0500

    Proyecto inicial

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+import express from 'express';
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
+import indexRouter from './router/index.js' //llamamos al router
+
+//Creamos la instancia
+const app = express();
+
+//Configurar EJS como el controlador de vistas
+app.set('view engine', 'ejs')
+
+//Ruta para la pagina principal, ruteo carpeta 'views'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.set('views', join(__dirname,'views'))
+
+app.use(indexRouter) //le decimos a Express que utilice las rutas definidas en router
+
+app.use(express.static(join(__dirname,'public'))) // llamando al archivo CSS
+
+
+//Inicial el servidor
+app.listen(3000)
+console.log("El servidor está siendo escuchado en el puerto: ", 3000)
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+const router = Router()
+
+//De la raiz renderiza un archivo llamado index.ejs
+router.get('/', (req, res) =>{
+    res.render('index',{title: 'Mi primer proyecto en NodeJS'});
+});
+router.get('/about', (req, res) =>{
+    res.render('about',{title: 'About us'});
+});
+router.get('/contact', (req, res) =>{
+    res.render('contact',{title: 'Contactanos'});
+});
+
+export default router
