070088bad4b4c7ea03923d0a0e92ff4c453743ac
williamvillacrez <william.villacrez89@gmail.com>
2024-10-13 16:54:11 -0500

    Actualizacion 13/10

--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,23 @@ import { Router } from 'express'
 const router = Router()
 
 //De la raiz renderiza un archivo llamado index.ejs
-router.get('/', (req, res) =>{
-    res.render('index',{title: 'Mi primer proyecto en NodeJS'});
+router.get('/index', (req, res) =>{
+    res.render('index',{title: 'Ignite Gym'});
 });
-router.get('/about', (req, res) =>{
-    res.render('about',{title: 'About us'});
+router.get('/servicios', (req, res) =>{
+    res.render('servicios',{title: 'Servicios'});
 });
-router.get('/productos', (req, res) =>{
-    res.render('productos',{title: 'Productos'});
+router.get('/planes', (req, res) =>{
+    res.render('planes',{title: 'Planes'});
 });
-router.get('/login', (req,res) =>{
-    res.render('login',{title:'Login'})
-})
-router.get('/producto', (req, res) => {
+router.get('/productos', (req, res) => {
     const productos = [
         { nombre: 'Producto 1', imagen: '/img/producto1.png' },
         { nombre: 'Producto 2', imagen: '/img/producto2.png' },
     ];
-    res.render('producto', { title: 'Productos', productos });
+    res.render('productos', { title: 'Productos', productos });
 });
+router.get('/login', (req,res) =>{
+    res.render('login',{title:'Login'})
+})
 export default router
