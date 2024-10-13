35e91298c7dbf64d8d6817c764758159b31f1fa2
Alexnarea <lanarea@sudamericano.edu.ec>
2024-10-13 14:11:34 -0500

    update product

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,26 @@ import { Router } from 'express'
 const router = Router()
 
 //De la raiz renderiza un archivo llamado index.ejs
-router.get('/', (req, res) =>{
+router.get('/inicio', (req, res) =>{
     res.render('index',{title: 'Mi primer proyecto en NodeJS'});
 });
-router.get('/about', (req, res) =>{
-    res.render('about',{title: 'About us'});
+router.get('/servicios', (req, res) =>{
+    res.render('servicios',{title: 'Servicios'});
 });
-router.get('/contact', (req, res) =>{
-    res.render('contact',{title: 'Contactanos'});
+router.get('/planes', (req, res) =>{
+    res.render('planes',{title: 'Planes'});
+});
+router.get('/productos', (req, res) =>{
+    res.render('productos',{title: 'Productos'});
 });
 router.get('/login', (req,res) =>{
     res.render('login',{title:'Login'})
 })
-
+router.get('/producto', (req, res) => {
+    const productos = [
+        { nombre: 'Producto 1', imagen: '/img/producto1.png' },
+        { nombre: 'Producto 2', imagen: '/img/producto2.png' },
+    ];
+    res.render('producto', { title: 'Productos', productos });
+});
 export default router
