61d8cd0b97782ecc66f19e87a264508e06d3a979
williamvillacrez <william.villacrez89@gmail.com>
2024-10-13 11:59:56 -0500

    vistas

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,17 @@ import { Router } from 'express'
 const router = Router()
 
 //De la raiz renderiza un archivo llamado index.ejs
-router.get('/', (req, res) =>{
-    res.render('index',{title: 'Mi primer proyecto en NodeJS'});
+router.get('/index', (req, res) =>{
+    res.render('index',{title: 'Ignite GYM'});
 });
-router.get('/about', (req, res) =>{
-    res.render('about',{title: 'About us'});
+router.get('/servicios', (req, res) =>{
+    res.render('servicios',{title: 'Servicios'});
 });
-router.get('/contact', (req, res) =>{
-    res.render('contact',{title: 'Contactanos'});
+router.get('/planes', (req, res) =>{
+    res.render('planes',{title: 'Planes'});
+});
+router.get('/productos', (req, res) =>{
+    res.render('productos',{title: 'Productos'});
 });
 router.get('/login', (req,res) =>{
     res.render('login',{title:'Login'})
